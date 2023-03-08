2d73ee13c884b3a33fa4c3b8bc7984e73b6cd98a
themockingjester <yashmathur123123@gmail.com>
2023-03-08 14:10:42 +0530

    fixed typo mistake

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
 // const { isAuthorisedForNormalTasks, isAuthorisedForSensitiveTasks, addAdminAuthCode } = require("../middlewares/securityMiddleware")
-const { registerNewUser } = require("../controllers/userController")
+const { registerNewUser } = require("../controllers/UserController")
 router.put('/registerUser', registerNewUser)
 module.exports = router
